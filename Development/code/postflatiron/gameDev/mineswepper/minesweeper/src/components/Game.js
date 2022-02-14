ae5c2acaacc0d69597f55ab9b64755c6cae32232
Madison <madivallarino@gmail.com>
2022-02-14 15:46:56 -0500

    whatever

---
+++ Development/code/postflatiron/gameDev/mineswepper/minesweeper/src/components/Game.js
@@ -0,0 +1,14 @@
+import GameBoard from './GameBoard';
+
+function Game(){
+
+
+
+    return (
+        <div className="gameboard">
+          <GameBoard /> 
+        </div>
+    )
+}
+
+export default Game
---
+++ Development/code/postflatiron/gameDev/mineswepper/minesweeper/src/components/GameBoard.js
@@ -0,0 +1,13 @@
+function GameBoard(){
+
+
+
+    return(
+
+        <div>
+            GameBoard
+        </div>
+    )
+}
+
+export default GameBoard;
