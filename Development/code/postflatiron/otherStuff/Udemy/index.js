f07cde50c6fbf0e3ea92d058c207077cd6a30127
Madison <madivallarino@gmail.com>
2022-03-02 16:00:00 -0500

    wednesdays work

--- Development/code/postflatiron/otherStuff/Udemy/index.js
+++ Development/code/postflatiron/otherStuff/Udemy/index.js
@@ -31,4 +31,76 @@
 
 
 
-console.log(selectionSort([8, 6, 4, 1, 2, 5]))
+// console.log(selectionSort([8, 6, 4, 1, 2, 5]))
+
+// function sumZero(arr){
+//     for(let i = 0; i < arr.length; i++){
+//         for(let j = i + 1; j < arr.length; j++){
+           
+//             if(arr[i] + arr[j] === 0){
+//                 return [arr[i], arr[j]]
+//             }
+//         }
+//     }
+//     return undefined 
+// }
+
+
+
+
+
+// let test1 = [-3, -2, -1, 0, 1, 2, 3]
+// let test2 = [-2, 0, 1, 3]
+// let test3 = [1, 2, 3]
+
+// console.log(sumZero(test3))
+
+
+// function countUniqueValues(arr){
+//     let counter = 0;
+//     let firstpointer = 0;
+//     let secondpointer = firstpointer + 1;
+
+//     while(firstpointer < arr.length ){
+//         if(arr[firstpointer] === arr[secondpointer]){
+//             secondpointer++
+//             firstpointer++
+//         } else if (arr[firstpointer] != arr[secondpointer]){
+//             counter++
+//             firstpointer++ 
+//             secondpointer = firstpointer + 1
+//         }
+    
+        
+//     }
+//     return counter
+// }
+
+
+
+function countUniqueValues(arr){
+    let i = 0;
+    let j = 1;
+    while(j < arr.length  ){
+        if(arr[i] === arr[j]){
+            j++
+        }else {
+            i++
+           arr[i] = arr[j]
+           
+           j++
+        }
+    
+    }
+    return i +1 
+}
+
+let test1 = [1, 1, 1, 1, 1, 2]
+let test2 = [1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]
+let test3 = []
+let test4 = [-2, -1, -1, 0, 1]
+let test5 = [1, 2, 3, 4, 4, 4]
+
+
+
+console.log(countUniqueValues(test2))
