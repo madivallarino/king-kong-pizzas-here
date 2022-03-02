ff442d1f25bee4f47ab8402cb27a126c95309f10
Madison <madivallarino@gmail.com>
2022-03-02 14:36:01 -0500

    indexjs

---
+++ Development/code/postflatiron/otherStuff/Udemy/index.js
@@ -0,0 +1,34 @@
+// function sort(arr){
+    
+//     for(let i = 0; i < arr.length; i++){
+//         for(let j = 0; j < arr.length; i++){
+//             if(arr[j] > arr[j + 1]){
+//                 let temp = arr[j]
+//                 arr[j] = arr[j + 1]
+//                 arr[j + 1] = temp
+                
+//             }
+//         }
+//     }
+// return arr
+
+// }
+
+// function selectionSort(arr){
+
+//     for(let i = 0; i < arr.length; i++){
+//         for(let j = 0; j < arr.length; j++){
+//             if(arr[j] > arr[j+ 1]){
+//                 let temp = arr[j]
+//                 arr[j] = arr[j + 1]
+//                 arr[j + 1] = temp
+//             }
+//         }
+//     }
+// return arr
+
+// }
+
+
+
+console.log(selectionSort([8, 6, 4, 1, 2, 5]))
